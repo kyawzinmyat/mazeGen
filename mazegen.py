f90b6e979e2c78852e1befea9376b217b35fd338
kyawzinmyat <kyawzinmyat00000001@gmail.com>
2022-06-11 23:47:13 -0800

    done

--- mazegen.py
+++ mazegen.py
@@ -5,34 +5,38 @@ class Maze:
 	def __init__(self):
 		self.maze =[
 		]
-		self.wall ="■"	
-		
+		self.wall ="#"
+		self.space = " "	
 	
 	def generate_maze(self,row,col):
 		self.maze =[[Cell([i,j]) for j in range(col)]for i in range(row) ]
+		self.maze_array =[[0 for j in range(3*col)]for i in range(3*row) ]
 		self.dfs()
+		self.convert_array()
 	
 	
 	def dfs(self):
-		self.visited_cells =[]	
+		self.visited_cells =[]
+		self.visited_index = []	
 		for i in range(len(self.maze)):
 			for j in range(len(self.maze[0])):
 				obj = self.maze[i][j]
-				if obj not in self.visited_cells:
+				if obj not in self.visited_cells and [i,j] not in self.visited_index:
 					self.visited_cells.append(obj)	
-					#try:	
+					self.visited_index.append([i,j])	
+					#print(i,j)
 					next_cell= self.connect_cell(obj)
 					next_cell_obj = self.maze[next_cell[0]][next_cell[1]]
 					next_cell_obj.wall[obj.op[next_cell[2]]]=False
 					obj.wall[next_cell[2]]=False
 				
 					self._dfs(obj,self.maze[next_cell[0]][next_cell[1]])
-			#		except:
-				#		pass
 				
 	
 	def _dfs(self,pre_cell,current_cell):
 		self.visited_cells.append(current_cell)
+		#print(current_cell.index_)
+		self.visited_index.append(current_cell.index_)
 		next_cell= self.connect_cell(current_cell)
 		pre_cell.wall[next_cell[2]]=False
 		current_cell.wall[current_cell.op[next_cell[2]]]=False
@@ -42,23 +46,22 @@ class Maze:
 		
 
 	def connect_cell(self,cell_to_connect):
-		next_cell= random.choice(self.get_index(cell_to_connect.index_))
-		return next_cell
+		next_cells = []
+		for cell in self.get_index(cell_to_connect.index_):
+				if cell not in self.visited_index:
+					#print("cell",cell)
+					next_cells.append(cell)
+		return random.choice(next_cells)
 		
 			
 		
 		
 	def print(self,custom_maze=None):
-		maze = self.maze
-		if custom_maze:
-			maze = custom_maze
-		
-		for i in maze:
-			k=""
-			for j in i:
-				k+="".join(j)
-			print(k)
-			
+		for cells in self.maze_array:
+			for cell in cells:
+				print(cell,end="")
+			print(end='\n')
+
 	def get_index_of(self,node):
 		for i,x in enumerate(self.maze):
 			for j,y in enumerate(x):
@@ -73,26 +76,74 @@ class Maze:
 		if index[0]+1<row_range:#down
 				#if self.maze[index[0]+1][index[1]]==self.wall or self.maze[index[0]+1][index[1]]==self.stop:
 					list_of_index.append([index[0]+1,index[1],"bottom"])
-					print(index,"bottom")
+					#print(index,"bottom")
 		
 		if index[1]-1>=0:##left
 			#if self.maze[index[0]][index[1]-1]== self.wall or self.maze[index[0]][index[1]-1]==self.stop: 
-					print(index,"left")
+					#print(index,"left")
 					list_of_index.append([index[0],index[1]-1,"left"])
 					
 		
 		if index[0]-1>=0:#up
 			#if self.maze[index[0]-1][index[1]]== self.wall or self.maze[index[0]-1][index[1]]==self.stop:
 					list_of_index.append([index[0]-1,index[1],"top"])
-					print(index,"up")
+					#print(index,"up")
+
 		if index[1]+1<col_range:#right
 			#if self.maze[index[0]][index[1]+1]==self.wall  or self.maze[index[0]][index[1]+1]==self.stop:
 					list_of_index.append([index[0],index[1]+1,"right"])
-					print(index,"right")
+					#print(index,"right")
 									
 		return list_of_index
 
+	def convert_array(self):
+		self.maze_array = []
+		for index,cells in enumerate(self.maze):
+			row = [self.wall for i in range(len(self.maze[0])*3)]
+			row2 = [self.wall for i in range(len(self.maze[0])*3)]
+			row3 = [self.wall for i in range(len(self.maze[0])*3)]
+			for index2,cell in enumerate(cells):
+				start = index2*3
+				end = index2*3 +3
+				mid = int((start+end)/2)
+				
+				row2[mid] = self.space
+				self.remove_wall(start,mid,cell,row,row2,row3)
+			self.maze_array.append(row)
+			self.maze_array.append(row2)
+			self.maze_array.append(row3)
+
+
+	def remove_wall(self,start,mid,cell,row,row2,row3):
+		if not cell.wall["left"]:
+
+			row2[start]=self.space
+		if not cell.wall["right"]:
+			row2[start+2]=self.space
+		if not cell.wall["top"]:
+			row[mid]=self.space
+		if not cell.wall['bottom']:
+			row3[mid] =self.space
+
+	
+	def start(self,start,index):
+		self.start = start
+		self.maze_array[index[0]][index[1]] = self.start 
+
+	def stop(self,stop,index):
+		self.stop = stop
+		self.maze_array[index[0]][index[1]] = self.stop
 		
+		
+
+
+				
+
+
+
+			
+
+
 class Cell:
 	def __init__(self,index):
 		self.x= index[0]
@@ -115,11 +166,11 @@ class Cell:
 												
 		
 maze = Maze()
-maze.generate_maze(4,4)
+maze.generate_maze(7,30)
+maze.print()
 #maze.
 
-for i in maze.maze:
-	k = [j.wall for j in i]
-	print(k)
 
-
+##for cells in maze.maze:
+##	for cell in cells:
+##		print(cell.wall)
