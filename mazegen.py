6a89e0e997cff5f7e96d1ebb8f543f3169f1cb3a
kyawzinmyat <72294831+kyawzinmyat@users.noreply.github.com>
2022-08-23 19:25:00 +0630

    refactored and remove duplicate code

--- mazegen.py
+++ mazegen.py
@@ -27,7 +27,6 @@ class Maze:
 			self.maze_array[(end[0])*3-1][end[1]*3-2] = 'E'
 			self.dfs.set_maze(self.maze_array[:])
 			while not self.dfs.solve(fill=False):	
-				print(counter)	
 				self.dfs2(seed,end)
 				self.convert_array()
 				self.maze_array[random.randint(0,row)][random.randint(0,col)] = 'A'
@@ -36,42 +35,19 @@ class Maze:
 				if counter > 100:
 					break
 		
-		return
+		return self.maze_array
+
+	def generate_maze_web(self, row, col, seed = None, end = None, fill = True):
+		self.maze =[[Cell([i,j]) for j in range(col)]for i in range(row) ]
+		self.dfs2(seed,end)
+		return self.maze
 	
 	
-	def dfs1(self):
-		for i in range(len(self.maze)):
-			for j in range(len(self.maze[0])):
-				obj = self.maze[i][j]
-				if obj not in self.visited_cells and [i,j] not in self.visited_index and [i,j] not in self.visited_index:
-					self.visited_cells.append(obj)	
-					self.visited_index.append([i,j])	
-					#print("current cell",i,j)
-					next_cell= self.connect_cell(obj)
-					next_cell_obj = self.maze[next_cell[0]][next_cell[1]]
-					obj.wall[next_cell[2]]=False
-					next_cell_obj.wall[obj.op[next_cell[2]]]=False
-					#print(obj.wall)
-				
-					self._dfs(obj,next_cell_obj)
-				
-	
-	def _dfs(self,pre_cell,current_cell):
-		self.visited_cells.append(current_cell)
-		self.visited_index.append(current_cell.index_)
-		next_cell= self.connect_cell(current_cell)
-		pre_cell.wall[next_cell[2]]=False
-		current_cell.wall[current_cell.op[next_cell[2]]]=False
-		if self.maze[next_cell[0]][next_cell[1]] not in self.visited_cells and next_cell not in self.visited_index:
-			self._dfs(current_cell,self.maze[next_cell[0]][next_cell[1]])
-		return 		
-		
 
 	def connect_cell(self,cell_to_connect):
 		next_cells = []
 		for cell in self.get_index(cell_to_connect.index_):
 				if cell not in self.visited_index:
-					#print("cell",cell)
 					next_cells.append(cell)
 		return random.choice(next_cells)
 
@@ -79,10 +55,6 @@ class Maze:
 		cells = []
 		for cell in self.get_index(cell_to_connect.index_):
 				if cell not in self.visited_index:
-					#cells.append(cell)
-		#random.shuffle(cells)
-		#for cell in cells:
-		#		print(cell)			
 					self.stack.appendleft(cell)
 
 		
